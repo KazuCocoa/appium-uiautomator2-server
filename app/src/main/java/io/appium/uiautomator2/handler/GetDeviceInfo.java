9b8db0dcdaa1e6ce500be5023ca0a63c24b7d3d7
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2019-11-09 18:45:01 +0900

    remove networkinfo

--- app/src/main/java/io/appium/uiautomator2/handler/GetDeviceInfo.java
+++ app/src/main/java/io/appium/uiautomator2/handler/GetDeviceInfo.java
@@ -16,14 +16,24 @@
 
 package io.appium.uiautomator2.handler;
 
+import android.Manifest;
 import android.app.Instrumentation;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.net.ConnectivityManager;
 import android.net.Network;
 import android.net.NetworkCapabilities;
 import android.net.NetworkInfo;
+import android.telephony.TelephonyManager;
+
+import androidx.test.platform.app.InstrumentationRegistry;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import io.appium.uiautomator2.common.exceptions.UiAutomator2Exception;
 import io.appium.uiautomator2.handler.request.SafeRequestHandler;
@@ -51,23 +61,104 @@ public class GetDeviceInfo extends SafeRequestHandler {
         }
     }
 
+    private static String[] TRANSPORT_NAMES = {
+            "CELLULAR",
+            "WIFI",
+            "BLUETOOTH",
+            "ETHERNET",
+            "VPN",
+            "WIFI_AWARE",
+            "LOWPAN",
+            "TEST"
+    };
+
+    // A note for For API 30+
+    private static List<String> getTransport(NetworkCapabilities networkCapabilities) {
+        // https://android.googlesource.com/platform/frameworks/base/+/master/core/java/android/net/NetworkCapabilities.java#702
+        List<String> transport = new ArrayList<>();
+
+        int index;
+        for (index = 0; index < TRANSPORT_NAMES.length; index++) {
+            if (networkCapabilities.hasTransport(index)) {
+                transport.add(String.valueOf(index));
+                transport.add(TRANSPORT_NAMES[index]);
+                return transport;
+            }
+        }
+        transport.add(String.valueOf(index));
+        transport.add("UNKNOWN");
+        return transport;
+    }
+
+    private static String getTelephonyCondition(int dataState) {
+        switch (dataState) {
+        case TelephonyManager.DATA_DISCONNECTED:
+            return "DISCONNECTED";
+        case TelephonyManager.DATA_CONNECTING:
+            return "CONNECTING";
+        case TelephonyManager.DATA_CONNECTED:
+            return "CONNECTED";
+        case TelephonyManager.DATA_SUSPENDED:
+            return "SUSPENDED";
+        default:
+            return "UNKNOWN";
+        }
+    }
+
     private static JSONArray extractNetworkInfo(DeviceInfoHelper deviceInfoHelper) throws JSONException {
         JSONArray result = new JSONArray();
         for (Network network : deviceInfoHelper.getNetworks()) {
             JSONObject resultItem = new JSONObject();
-            NetworkInfo networkInfo = deviceInfoHelper.extractInfo(network);
-            if (networkInfo != null) {
-                resultItem.put("type", networkInfo.getType());
-                resultItem.put("typeName", networkInfo.getTypeName());
-                resultItem.put("subtype", networkInfo.getSubtype());
-                resultItem.put("subtypeName", networkInfo.getSubtypeName());
-                resultItem.put("isConnected", networkInfo.isConnected());
-                resultItem.put("detailedState", networkInfo.getDetailedState().name());
-                resultItem.put("state", networkInfo.getState().name());
-                resultItem.put("extraInfo", formatNull(networkInfo.getExtraInfo()));
-                resultItem.put("isAvailable", networkInfo.isAvailable());
-                resultItem.put("isFailover", networkInfo.isFailover());
-                resultItem.put("isRoaming", networkInfo.isRoaming());
+
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q) {
+                Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
+                TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+                ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+                if (context.checkSelfPermission(Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+                    if (telephonyManager != null) {
+                        resultItem.put("cellState", getTelephonyCondition(telephonyManager.getDataState()));
+                        if (telephonyManager.getSignalStrength() != null) {
+                            // signal strength of 'data network' (Celler)
+                            resultItem.put("cellSignalStrength", telephonyManager.getSignalStrength().getLevel());
+                        }
+                    }
+
+                    if (connectivityManager != null)
+                        connectivityManager.registerDefaultNetworkCallback(new ConnectivityManager.NetworkCallback());
+                        Network netw = connectivityManager.getActiveNetwork();
+
+                        NetworkCapabilities networkCapabilities = connectivityManager.getNetworkCapabilities(netw);
+                        if (networkCapabilities != null) {
+                            List<String> transport = getTransport(networkCapabilities);
+                            resultItem.put("type", transport.get(0));
+                            resultItem.put("typeName", transport.get(1));
+                        }
+                        // Return whether the data network is currently active
+//                        connectivityManager.isDefaultNetworkActive();
+//                        resultItem.put("isConnected", networkInfo.isConnected());
+//
+//                        resultItem.put("detailedState", networkInfo.getDetailedState().name());
+//                        resultItem.put("state", networkInfo.getState().name());
+//                        resultItem.put("isAvailable", networkInfo.isAvailable());
+//                        resultItem.put("isFailover", networkInfo.isFailover());
+//                        resultItem.put("isRoaming", networkInfo.isRoaming());
+                }
+            } else {
+                NetworkInfo networkInfo = deviceInfoHelper.extractInfo(network);
+                if (networkInfo != null) {
+                    resultItem.put("type", networkInfo.getType());
+                    resultItem.put("typeName", networkInfo.getTypeName());
+                    resultItem.put("subtype", networkInfo.getSubtype());
+                    resultItem.put("subtypeName", networkInfo.getSubtypeName());
+                    resultItem.put("isConnected", networkInfo.isConnected());
+                    resultItem.put("detailedState", networkInfo.getDetailedState().name());
+                    resultItem.put("state", networkInfo.getState().name());
+                    resultItem.put("extraInfo", formatNull(networkInfo.getExtraInfo()));
+                    resultItem.put("isAvailable", networkInfo.isAvailable());
+                    resultItem.put("isFailover", networkInfo.isFailover());
+                    resultItem.put("isRoaming", networkInfo.isRoaming());
+                }
             }
 
             NetworkCapabilities networkCaps = deviceInfoHelper.extractCapabilities(network);
